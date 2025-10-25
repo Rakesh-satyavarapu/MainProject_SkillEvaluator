64819724dbcc3af7c39c84aeaad84737a89d7da3
Rakesh-satyavarapu <rakeshsatyavarapu369@gmail.com>
2025-10-25 18:32:52 +0530

    Update forgotPass.router.js

--- server/routes/forgotPass.router.js
+++ server/routes/forgotPass.router.js
@@ -1,5 +1,5 @@
 let express = require('express');
-let { findUser, saveOtp, confirmOtp, setPassword } = require('../controllers/forgotPass.controller');
+let { findUser, saveOtp, confirmOtp, setPassword } = require('../controllers/forgotPass.controller.js');
 
 let router = express.Router();
 
