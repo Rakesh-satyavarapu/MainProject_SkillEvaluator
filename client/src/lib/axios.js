649e40a440ed473bbec0676b4e7375db81de15a7
Rakesh-satyavarapu <rakeshsatyavarapu369@gmail.com>
2025-11-04 22:05:17 +0530

    added changed frontend ui

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL:"https://aiskillevaluator.onrender.com",
-    baseURL:"http://localhost:5000",
+    baseURL:"https://aiskillevaluator.onrender.com",
     withCredentials:true
+    // baseURL:"http://localhost:5000",
 })
--- server/app.js
+++ server/app.js
@@ -22,15 +22,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-// app.use(cors({
-//     origin: 'https://skillsevaluator.onrender.com',
-//     credentials: true
-// }))
-
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://skillsevaluator.onrender.com',
     credentials: true
 }))
+
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true
+// }))
 
 app.use('/auth',authRoutes)
 app.use('/user',protectedRoute,userRoutes)
